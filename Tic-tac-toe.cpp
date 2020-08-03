d5394fb77142aec8d00ecd6674de895effc6ee20
DEV <46674147+dev7060@users.noreply.github.com>
2020-08-03 18:13:24 +0530

    Create Tic-tac-toe.cpp

---
+++ Tic-tac-toe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int arr[9]={9, 9, 9, 9, 9, 9, 9, 9, 9};
+void update(){
+    
+    cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<"\n";
+    cout<<arr[3]<<" "<<arr[4]<<" "<<arr[5]<<"\n";
+    cout<<arr[6]<<" "<<arr[7]<<" "<<arr[8]<<"\n";
+}
+ 
+int main()
+{   update();
+    
+    return 0;
+}
